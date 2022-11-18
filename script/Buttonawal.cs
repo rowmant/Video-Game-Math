2d77aef2b0ae46e5dd85502403019c9c080b96f5
rowmant <118442707+rowmant@users.noreply.github.com>
2022-11-18 20:52:36 +0700

    game video matematika
    sound nya belum dibuat, mas

---
+++ script/Buttonawal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonawal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void scale(float scale)
+    {
+        transform.localScale = new Vector2(1 / scale, 1 * scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ script/LevelManagerList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManagerList : MonoBehaviour
+{
+    public void LoadToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
